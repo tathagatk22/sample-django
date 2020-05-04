c473287290826ee5eab92a09d86c2ac3de8592df
tathagatk22 <tathagat.khanorkar@gmail.com>
2020-05-04 21:13:56 +0530

    Added urls for core app with health-check URL

--- core/views.py
+++ core/views.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.decorators import api_view
+
+
+@api_view(['GET'])
+def health_check(request):
+    response = {"Status": True}
+    return JsonResponse(response, safe=False)
