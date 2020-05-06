1a5e2e470bd3b78c6a98e06db34d73ff5879b46a
tathagatk22 <tathagat.khanorkar@gmail.com>
2020-05-06 19:58:59 +0530

    removing /.idea removing Dockerfile updating settings.py with db.sqlite3 path updating requirements.txt with gunicorn

--- django_sample_project/settings.py
+++ django_sample_project/settings.py
@@ -74,7 +74,7 @@ WSGI_APPLICATION = 'django_sample_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'local_db', 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
